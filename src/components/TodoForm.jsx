569d20a0177c19883cb5d985eeeba7d45c8ac832
Sumin-Lee12 <leesumin041@gmail.com>
2025-03-07 12:59:29 +0900

    이전 커밋으로 오류가 나서 재수정

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,46 +1,46 @@
-// import { useState } from "react";
-// import { todoApi } from "../api/todos";
+import { useState } from "react";
+import { todoApi } from "../api/todos";
 
-// export default function TodoForm({ fetchData }) {
-//   const [title, setTitle] = useState("");
-//   const [contents, setContents] = useState("");
+export default function TodoForm({ fetchData }) {
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
 
-//   // TODO: 필수: useMutation 으로 리팩터링 하세요.
-//   // TODO: 선택: useMutation 으로 리팩터링 후, useTodoMutation 커스텀훅으로 정리해 보세요.
-//   const handleAddTodo = async (e) => {
-//     e.preventDefault();
-//     setTitle("");
-//     setContents("");
-//     await todoApi.post("/todos", {
-//       id: Date.now().toString(),
-//       title,
-//       contents,
-//       isCompleted: false,
-//       createdAt: Date.now(),
-//     });
-//     await fetchData();
-//   };
+  // TODO: 필수: useMutation 으로 리팩터링 하세요.
+  // TODO: 선택: useMutation 으로 리팩터링 후, useTodoMutation 커스텀훅으로 정리해 보세요.
+  const handleAddTodo = async (e) => {
+    e.preventDefault();
+    setTitle("");
+    setContents("");
+    await todoApi.post("/todos", {
+      id: Date.now().toString(),
+      title,
+      contents,
+      isCompleted: false,
+      createdAt: Date.now(),
+    });
+    await fetchData();
+  };
 
-//   return (
-//     <form onSubmit={handleAddTodo}>
-//       <label htmlFor="title">제목:</label>
-//       <input
-//         type="text"
-//         id="title"
-//         name="title"
-//         value={title}
-//         onChange={(e) => setTitle(e.target.value)}
-//         required
-//       />
-//       <label htmlFor="contents">내용:</label>
-//       <input
-//         id="contents"
-//         name="contents"
-//         value={contents}
-//         onChange={(e) => setContents(e.target.value)}
-//         required
-//       />
-//       <button type="submit">추가하기</button>
-//     </form>
-//   );
-// }
+  return (
+    <form onSubmit={handleAddTodo}>
+      <label htmlFor="title">제목:</label>
+      <input
+        type="text"
+        id="title"
+        name="title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        required
+      />
+      <label htmlFor="contents">내용:</label>
+      <input
+        id="contents"
+        name="contents"
+        value={contents}
+        onChange={(e) => setContents(e.target.value)}
+        required
+      />
+      <button type="submit">추가하기</button>
+    </form>
+  );
+}
